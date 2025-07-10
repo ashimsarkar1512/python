d8d9477776b33f50803d43054cb68285ae63536b
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-11 03:26:35 +0600

    learn to assignment  operators

---
+++ arrithmeticOperators.py
@@ -0,0 +1,10 @@
+# arrithmetic operators
+a=10
+b=5
+
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+print (a%b)
+print (a**b)
---
+++ assignmentOperators.py
@@ -0,0 +1,21 @@
+sum=10
+sum +=10
+print("sum:", sum)
+
+
+multiple=20
+multiple *=10
+print("multiple:", multiple)
+
+divided=20
+divided /=10
+print("divided:", divided)
+
+modular=20
+modular %=6
+print("modular:", modular)
+
+
+power=3
+power **=2
+print("power:", power)
