4d29eab087e86b1360d4c339d51b13509259dff5
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-11 06:11:44 +0600

    slove the practice task

---
+++ practiceTask.py
@@ -0,0 +1,19 @@
+first=int(input("enter your first number :"))
+second=int(input("enter your second number :"))
+
+print("sum =",first+second)
+
+
+side = float(input("inter side squre :"))
+
+print("area =", side**2)
+
+
+num1=float(input("enter first floating number :"))
+num2=float(input("enter second floating number :"))
+print("average =", (num1+num2)/2)
+
+a=int(input("enter value  a  number :"))
+b=int(input("enter value  b  number :"))
+
+print("result =",(a>=b))
