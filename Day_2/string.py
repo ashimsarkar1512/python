e477a1454bcbeb3954c15d900a26d4a9f604211f
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-13 06:09:02 +0600

    learn to string lenth

---
+++ Day_2/string.py
@@ -0,0 +1,14 @@
+str1="ashim\nkumarsarkar"
+print(str1)
+str2="ashim\tkumarsarkar"
+print(str2)
+
+str3="ashim"
+str4="kumarsarkar"
+join=str3+str4
+print(join)
+
+name="ashimkumarsarkar"
+lenth=len(name)
+print(lenth)
+
