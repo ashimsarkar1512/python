050d7b67c0cd5fd99307b2fcfca30b9b854fc5ad
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-13 06:29:06 +0600

    learn to string lenth

---
+++ Day_2/index.py
@@ -0,0 +1,4 @@
+str="ashimkumarsarkar"
+index=str[5]
+print(index)
+print(str[9])
---
+++ Day_2/slice.py
@@ -0,0 +1,13 @@
+name="ashimkumarsarkar"
+slice=name[0:5]
+print(slice)
+
+print(name[5:10])
+sob=name[0:len(name)]
+print(sob)
+
+
+desh="bangladesh"
+
+print(desh[0:len(desh)])
+
