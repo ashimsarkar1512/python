8874644def4f6d81dcafa2ebcd2212b9cba59621
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-15 00:54:50 +0600

    slove the practice task

---
+++ Day_2/practicetask2.py
@@ -0,0 +1,12 @@
+# name=input("enter your name:")
+
+
+# print("name lenth:",len(name))
+
+
+dollarCount="$i am a $ student in $ northern university"
+print(dollarCount.count("$"))
+
+print(dollarCount.find("am"))
+
+print(dollarCount.replace("i","he"))
