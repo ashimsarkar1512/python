199b99c3e54dc4f4d38266a53c35e214754ca560
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-17 02:59:33 +0600

    nested dictionary learn

--- Day_4/dictionary.py
+++ Day_4/dictionary.py
@@ -7,3 +7,30 @@ info={
     "is_adult":True
 }
 print(info)
+
+print(info["name"])
+
+info["name"]="prity"
+print(info)
+
+
+name={}
+
+name["firstname"]="Ashim"
+name["surname"]="kumar"
+name["age"]=23
+name["education"]="bsc in cse"
+name["department"]="computer science and engineering"
+print(name)
+
+
+studentName={
+    "name":"ashim kumar sarkar",
+    "marks":{
+        "phy":80,
+        "chemistry":89.5,
+        "bangla":100,
+    }
+}
+
+print(studentName)
