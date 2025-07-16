5deeee1a9d1bf4edfcffb3d568832b6f7c9629ee
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-17 04:34:32 +0600

    create the set folder and learn to the set

---
+++ Day_4/set.py
@@ -0,0 +1,20 @@
+collection ={1,2,2,2,3,3,4,5,"hello","prity"}
+
+print(type(collection))
+print(len(collection))
+
+collection.add(9)
+print(collection)
+
+collection.add(("ashim",1,3))
+print(collection)
+collection.remove(3)
+print(collection)
+
+collection.clear()
+print(collection)
+
+
+collectionData={"prity","mukti","mritra","dighi","ashim"}
+collectionData.pop()
+print(collectionData)
