8effbc355c2133eade78b13a24bd0456779d9b1a
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-18 05:34:03 +0600

    practice

---
+++ Day_5/Loop/practice.py
@@ -0,0 +1,40 @@
+for i in range(1,101):
+    print(i)
+
+
+for ele in range(100,0,-1):
+    print(ele)
+
+
+n=int(input("enter your num:"))   
+
+for i in range(1,11):
+    print(n*i)
+
+
+n=7
+sum=0
+i=1
+
+while i<=6:
+    sum=i+sum
+    i +=1
+print(sum)   
+
+
+
+num=5
+fact=1
+i=1
+
+for i in range(1, num+1):
+    fact *=i
+
+print(fact)
+
+
+
+while i<=5:
+    fact*=i
+    i +=1
+print(fact)    
