ff7911b163d4ae52772f3d1956202102a9d48991
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-17 23:44:05 +0600

    learn to while loop

--- Day_5/Loop/while.py
+++ Day_5/Loop/while.py
@@ -1,9 +1,30 @@
-number=1
-while number<=100:
-    print(number)
-    number+=1
+# number=1
+# while number<=100:
+#     print(number)
+#     number+=1
 
-number=100
-while number >= 1:
-    print(number)
-    number-=1
+# number=100
+# while number >= 1:
+#     print(number)
+#     number-=1
+
+
+
+
+# number=2
+
+# while number<=50:
+#    print(number)
+#    number+=2
+
+# i=1
+# while i<=50:
+#    if i%2==0 :
+#     print(i)
+#    i +=1
+
+user = input("Enter your input (type 'exit' to stop): ")
+
+while user != "exit":
+    print("Hello, user!")
+    user = input("Enter your input (type 'exit' to stop): ")
