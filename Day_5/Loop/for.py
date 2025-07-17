d8deeeb23696a93b2b00787933195b2e5e787147
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-18 04:02:03 +0600

    create the for loop foldar and learn

---
+++ Day_5/Loop/for.py
@@ -0,0 +1,21 @@
+gf=["mritra","mukti","dighi","prity"]
+
+for ele in gf:
+    print(ele)
+
+
+roll=(23,34,55,67,89,97)
+
+for element in roll:
+    print(element)
+
+
+name="ashimkumarsarkar"    
+
+for char in name:
+    if(char=="r"):
+        break
+    print(char)
+
+else:
+        print("End")
