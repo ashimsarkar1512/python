3d9ff4f9b9f5c089b7d67611609df698415d6fc1
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-18 01:49:13 +0600

    learn to while loop

--- Day_5/Loop/while.py
+++ Day_5/Loop/while.py
@@ -1,54 +1,67 @@
-number=1
-while number<=100:
-    print(number)
-    number+=1
+# number=1
+# while number<=100:
+#     print(number)
+#     number+=1
 
-number=100
-while number >= 1:
-    print(number)
-    number-=1
-
-
-
-
-number=2
-
-while number<=50:
-   print(number)
-   number+=2
-
-i=1
-while i<=50:
-   if i%2==0 :
-    print(i)
-   i +=1
-
-user = input("Enter your input (type 'exit' to stop): ")
-
-while user != "exit":
-    print("Hello, user!")
-    user = input("Enter your input (type 'exit' to stop): ")
+# number=100
+# while number >= 1:
+#     print(number)
+#     number-=1
 
 
 
-# question 3
 
-n=int(input("enter number :"))
-i=1
-while i <=10:
-    print(n*i)
-    i +=1
+# number=2
+
+# while number<=50:
+#    print(number)
+#    number+=2
+
+# i=1
+# while i<=50:
+#    if i%2==0 :
+#     print(i)
+#    i +=1
+
+# user = input("Enter your input (type 'exit' to stop): ")
+
+# while user != "exit":
+#     print("Hello, user!")
+#     user = input("Enter your input (type 'exit' to stop): ")
 
 
-nums=[1,4,9,16,25,36,49,64,100]    
-idx=0
-while idx<(len(nums)):
-   print(nums[idx])
-   idx +=1
+
+# # question 3
+
+# n=int(input("enter number :"))
+# i=1
+# while i <=10:
+#     print(n*i)
+#     i +=1
 
 
-gfName=["mritra","mukti","dighi","prity"]
-i=0
-while i<len(gfName):
-   print(gfName [i])
-   i +=1
+# nums=[1,4,9,16,25,36,49,64,100]    
+# idx=0
+# while idx<(len(nums)):
+#    print(nums[idx])
+#    idx +=1
+
+
+# gfName=["mritra","mukti","dighi","prity"]
+# i=0
+# while i<len(gfName):
+#    print(gfName [i])
+#    i +=1
+
+
+
+numberSearch=(1,4,9,16,25,36,49,64,100) 
+x=49   
+id=0
+while id<len(numberSearch):
+   if(numberSearch[id]==x):
+        print(id)
+   else:
+       print("finding...")     
+      
+   id +=1
