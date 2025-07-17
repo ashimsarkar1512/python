79a5ac3244e37e87c9f3af88ae5a892c5786d59a
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-18 03:50:00 +0600

    learn to while loop

--- Day_5/Loop/while.py
+++ Day_5/Loop/while.py
@@ -1,57 +1,57 @@
-# number=1
-# while number<=100:
-#     print(number)
-#     number+=1
+number=1
+while number<=100:
+    print(number)
+    number+=1
 
-# number=100
-# while number >= 1:
-#     print(number)
-#     number-=1
-
-
-
-
-# number=2
-
-# while number<=50:
-#    print(number)
-#    number+=2
-
-# i=1
-# while i<=50:
-#    if i%2==0 :
-#     print(i)
-#    i +=1
-
-# user = input("Enter your input (type 'exit' to stop): ")
-
-# while user != "exit":
-#     print("Hello, user!")
-#     user = input("Enter your input (type 'exit' to stop): ")
+number=100
+while number >= 1:
+    print(number)
+    number-=1
 
 
 
-# # question 3
 
-# n=int(input("enter number :"))
-# i=1
-# while i <=10:
-#     print(n*i)
-#     i +=1
+number=2
+
+while number<=50:
+   print(number)
+   number+=2
+
+i=1
+while i<=50:
+   if i%2==0 :
+    print(i)
+   i +=1
+
+user = input("Enter your input (type 'exit' to stop): ")
+
+while user != "exit":
+    print("Hello, user!")
+    user = input("Enter your input (type 'exit' to stop): ")
 
 
-# nums=[1,4,9,16,25,36,49,64,100]    
-# idx=0
-# while idx<(len(nums)):
-#    print(nums[idx])
-#    idx +=1
+
+# question 3
+
+n=int(input("enter number :"))
+i=1
+while i <=10:
+    print(n*i)
+    i +=1
 
 
-# gfName=["mritra","mukti","dighi","prity"]
-# i=0
-# while i<len(gfName):
-#    print(gfName [i])
-#    i +=1
+nums=[1,4,9,16,25,36,49,64,100]    
+idx=0
+while idx<(len(nums)):
+   print(nums[idx])
+   idx +=1
+
+
+gfName=["mritra","mukti","dighi","prity"]
+i=0
+while i<len(gfName):
+   print(gfName [i])
+   i +=1
 
 
 
@@ -65,3 +65,21 @@ while id<len(numberSearch):
        print("finding...")     
       
    id +=1
+
+
+i=0
+while i<=5:
+    if i==3:
+       break
+    print(i)
+
+    i +=1
+         
+
+continueNum=1
+while continueNum<=10:
+    if(continueNum%2 != 0 ):
+        continueNum +=1
+        continue
+    print(continueNum)
+    continueNum +=1
