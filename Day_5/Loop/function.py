68951ad756c63536fcdb1161ebd2fa1c48c92c94
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-19 22:23:59 +0600

    learning to function

--- Day_5/Loop/function.py
+++ Day_5/Loop/function.py
@@ -4,3 +4,18 @@ def multiple(a,b,c):
 
 solve=multiple(2,3,4)
 print(solve)
+
+
+
+def ageEquation(a,b,c):
+    if(a>=b and a>=c):
+        return ("a is a big brother")
+    elif(b>=c and b>=a):
+        return("b is a big brrother")
+    else:
+       return ("c is a big brother")
+    
+
+result=ageEquation(7,9,8)
+
+print(result)
