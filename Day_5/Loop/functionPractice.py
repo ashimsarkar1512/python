11ead7eac4bd3ea640a21819ae818780ba50c3d6
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-19 23:26:39 +0600

    create the practice function

--- Day_5/Loop/functionPractice.py
+++ Day_5/Loop/functionPractice.py
@@ -6,3 +6,27 @@ def print_len(list):
 
 print_len(cities)  
 print_len(heroes)  
+
+
+
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
+print_list(cities)  
+print()
+
+
+
+n=5
+fact=1
+for i in range(1,1+n):
+    fact *=i
+    print(fact)
+
+
+def cal_fac(n):
+    fact=1
+    for i in range(1,1+n):
+        fact *=i
+        print(fact)
+cal_fac(6)        
