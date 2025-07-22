2222500beb1b2c1b817ce96c8b4bb5aafbf2e93a
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-07-22 06:35:34 +0600

    learn to recurssion

--- Day_5/Loop/recursion.py
+++ Day_5/Loop/recursion.py
@@ -17,3 +17,29 @@ def fact(n):
 
 multi=fact(4)
 print(multi)
+
+
+
+def cal_sum(n):
+    if(n==0):
+        return 0
+    return cal_sum(n-1)+n
+
+sum=cal_sum(5)
+
+print(sum)
+
+
+
+
+def print_list(list,idx=0):
+    if(idx==len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+
+
+fruits=["mango","banana","apple","orange"]
+
+print_list(fruits)
+
