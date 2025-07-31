1c54169e24ba53ea7a8fec064adb32a397cdb4b9
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-08-01 03:44:53 +0600

    learn to r+,w+,a+

---
+++ pratice.py
@@ -0,0 +1,3 @@
+with open("practice.txt","w") as f:
+ f.write("Hi everyone\nwe are learning file I\o\n")
+ f.write("using java\ni like programing like java")
--- write.py
+++ write.py
@@ -1,7 +1,24 @@
-f=open("demos.txt","a")
+# f=open("demos.txt","a")
 
-f.write("amaro porano jaha chai tumi tai sondor o vubone akash batash ashim")
+# f.write("amaro porano jaha chai tumi tai sondor o vubone akash batash ashim")
 
 
 
-f.close()
+# f.close()
+
+
+with open("with.txt","r")as f:
+
+    read=f.read()
+
+    print (read)
+
+
+
+
+    import os
+
+    os.remove("write.txt")
+
+
+
