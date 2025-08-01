4fcbeb99a8f02473439e6432298157c8bcf1c071
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-08-01 06:43:53 +0600

    practicse to all :

--- funtionPractice.py
+++ funtionPractice.py
@@ -45,3 +45,15 @@ with open("number.txt","r")as f:
       else:
          num +=data[i]   
 
+
+count =0
+with open("number.txt", "r") as f :
+   data=f.read()
+
+   nums=data.split(",")
+   for val in nums:
+       if(int(val)%2 == 0):
+         count +=1
+print(count)         
+         
+
