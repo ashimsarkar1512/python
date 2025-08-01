85c67c07d58d22b4b947db5ed4672f6d0f8914ff
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-08-01 06:34:31 +0600

    practicse to all :

--- funtionPractice.py
+++ funtionPractice.py
@@ -31,3 +31,17 @@ def check_for_line_word():
       return -1    
    
 print(check_for_line_word())
+
+
+
+with open("number.txt","r")as f:
+   data=f.read()
+   print(data)
+   num=""
+   for i in range(len(data)):
+      if(data[i]==","):
+         print(int(num))
+         num=""
+      else:
+         num +=data[i]   
+
